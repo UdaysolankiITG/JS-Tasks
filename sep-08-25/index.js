b9e6a631c087890f595cf7ce8f08317c90e75d5e
UdaysolankiITG <udaysolanki@itgeeks.com>
2025-09-08 17:43:34 +0530

    add

--- sep-08-25/index.js
+++ sep-08-25/index.js
@@ -1,4 +1,38 @@
-console.log("helo world");
-for (let index = 0; index < 10; index++) {
-    console.log(index)
+import axios from 'axios'
+// const url = "https://jsonplaceholder.typicode.com/comments" //get with param postId : 1
+const url = "https://ai-agent-steel-ten.vercel.app/api/v1/Auth/Signup" //post
+// const url = "https://jsonplaceholder.typicode.com/posts/1" //get
+// axios(url).then((data)=>{
+//     console.log("data",data)
+// }).catch((err)=>{
+//     console.log(err)   
+// })
+
+// async function getTodos() {
+//     try {
+//         const data = await axios.get(url, {
+//             params: {
+//                 postId: 1
+//             }
+//         });
+//         console.log(data.data)
+//     } catch (error) {
+//         console.log("error", error)
+//     }
+// }
+
+async function signup() {
+    try {
+        const data = await axios.post(url, {
+            Name: "uday",
+            Email: "uday@gmail.com",
+            Password: "uday@0711"
+        });
+        console.log(data.data)
+    } catch (error) {
+        console.log("error", error.message)
+    }
 }
+
+  
+console.log(signup())
